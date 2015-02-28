847859824175b89a74e3c91a6c99b6f1d39d1e50
Kyllian Régis <kyllianregis@live.ca>
2015-02-27 20:01:58 -0500

    Scène principale + script vueTactic

---
+++ WonderJamH2015/Assets/Script/vueTactic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vueTactic : MonoBehaviour {
+	Vector3 pos;
+	// Use this for initialization
+	void Start () {
+		pos = this.gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKeyDown(KeyCode.V)) {
+			pos.x = 10;
+			this.gameObject.transform.position = pos;
+			Debug.Log(this.gameObject.transform.position);
+		}
+		if (Input.GetKeyUp (KeyCode.V)) {
+			
+		}
+	}
+}
