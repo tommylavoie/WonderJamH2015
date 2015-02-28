97087a7030012088ad806c9e5d1490844615b269
Kyllian Régis <kyllianregis@live.ca>
2015-02-27 21:29:23 -0500

    Modif script vueTactic et ajout model_character

--- WonderJamH2015/Assets/Script/vueTactic.cs
+++ WonderJamH2015/Assets/Script/vueTactic.cs
@@ -3,20 +3,28 @@ using System.Collections;
 
 public class vueTactic : MonoBehaviour {
 	Vector3 pos;
+
 	// Use this for initialization
 	void Start () {
 		pos = this.gameObject.transform.position;
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
 		if (Input.GetKeyDown(KeyCode.V)) {
-			pos.x = 10;
+			pos.x = 0f;
+			pos.y = 30.24f;
+			pos.z = 24.93f;
+			this.gameObject.transform.rotation = Quaternion.AngleAxis(90, Vector3.right);
 			this.gameObject.transform.position = pos;
-			Debug.Log(this.gameObject.transform.position);
 		}
 		if (Input.GetKeyUp (KeyCode.V)) {
-			
+			pos.x = 0f;
+			pos.y = 13.49f;
+			pos.z = -22.65f;
+			this.gameObject.transform.rotation = Quaternion.AngleAxis(22, Vector3.right);
+			this.gameObject.transform.position = pos;	
+
 		}
 	}
 }
