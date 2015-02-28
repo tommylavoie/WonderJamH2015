95598d8cb71c5d8803966f9bbe0b3e1afc433f4f
leorux94 <fripouille_16@hotmail.com>
2015-02-28 01:11:42 -0500

    Lancement des projectiles

---
+++ WonderJamH2015/Assets/Script/TowerProjectile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerProjectile : MonoBehaviour
+{
+
+    public GameObject target;
+    public float speed = 25;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
