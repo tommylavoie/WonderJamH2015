f511499e65855355f4429a8fd065e5b38a7eb25e
leorux94 <fripouille_16@hotmail.com>
2015-02-28 23:01:02 -0500

    Ajout du module de son + fichiers son et musique

--- WonderJamH2015/Assets/Script/SoundEffectScript.cs
+++ WonderJamH2015/Assets/Script/SoundEffectScript.cs
@@ -3,13 +3,96 @@ using System.Collections;
 
 public class SoundEffectScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public static SoundEffectScript Instance;
+
+    public AudioClip banana_foomp;
+    public AudioClip banana_splat;
+    public AudioClip fillette_ahahahah;
+    public AudioClip fillette_ohnon;
+    public AudioClip game_over;
+    public AudioClip growl_01;
+    public AudioClip growl_02;
+    public AudioClip growl_03;
+    public AudioClip growl_04;
+    public AudioClip next_wave;
+    public AudioClip tower_fwiiiin;
+    public AudioClip tower_rawr;
+    
+    // etc...
+
+    void Awake()
+    {
+        // Register the singleton
+        if (Instance != null)
+        {
+            Debug.LogError("Multiple instances of SoundEffectsScript!");
+        }
+        Instance = this;
+    }
+
+    public void MakeBanana_foompSound() {
+        MakeSound(banana_foomp);
+    }
+
+    public void MakeBanana_splatSound() {
+        MakeSound(banana_splat);
+    }
+
+    public void MakeFillette_ahahahahSound()
+    {
+        MakeSound(fillette_ahahahah);
+    }
+
+    public void MakeFillette_ohnonSound()
+    {
+        MakeSound(fillette_ohnon);
+    }
+
+    public void MakeGame_overSound()
+    {
+        MakeSound(game_over);
+    }
+
+    public void MakeGrowl_1Sound()
+    {
+        MakeSound(growl_01);
+    }
+
+    public void MakeGrowl_2Sound()
+    {
+        MakeSound(growl_02);
+    }
+
+    public void MakeGrowl_3Sound()
+    {
+        MakeSound(growl_03);
+    }
+
+    public void MakeGrowl_4Sound()
+    {
+        MakeSound(growl_04);
+    }
+
+    public void MakeNext_waveSound()
+    {
+        MakeSound(next_wave);
+    }
+
+    public void Maketower_fwiiiinSound()
+    {
+        MakeSound(tower_fwiiiin);
+    }
+
+    public void MakeTower_rawrSound()
+    {
+        MakeSound(tower_rawr);
+    }
+
+
+    // Play a given sound
+    private void MakeSound(AudioClip originalClip)
+    {
+        // As it is not 3D audio clip, position doesn't matter.
+        AudioSource.PlayClipAtPoint(originalClip, transform.position);
+    }
 }
